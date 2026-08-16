61f33eed928322dc50c87d27ee8ffaababa7ab58
shwetasaini12 <shwetasaini857@gmail.com>
2026-08-16 16:22:07 +0530

    setup-1

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+  /* change to ul, li      */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
